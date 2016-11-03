6855d9227388cd28c9c4803949e0bdedffaac2e1
Daniel Park <op7773hons@gmail.com>
2016-11-03 16:05:55 +0900

    add touch event to CameraPreview for auto focus

--- app/src/main/java/com/danielpark/camerasample/MainActivity.java
+++ app/src/main/java/com/danielpark/camerasample/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -61,6 +62,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             containerView.addView(cameraPreview);
 
             cameraPreview.setOnTakePictureListener(this);
+
+            cameraPreview.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            cameraPreview.autoFocus();
+                            return true;
+                    }
+                    return false;
+                }
+            });
 
         } catch (UnsupportedOperationException e){
             Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
